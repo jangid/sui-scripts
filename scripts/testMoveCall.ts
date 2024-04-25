402b567e765bb72eb05383ed0e9ac449aa2ddd73
Pankaj Jangid <pankaj@optimizory.com>
2024-04-25 18:19:45 +0530

    First commit

---
+++ scripts/testMoveCall.ts
@@ -0,0 +1,39 @@
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { getExecStuff } from "./utils";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const testMoveCall = async (amount: string | number) => {
+  const { address, keypair, suiClient } = getExecStuff();
+  const txb = new TransactionBlock();
+  txb.setGasBudget(700000000);
+
+  const amountInMist = BigInt(amount) * BigInt(1000000000);
+  const [depositCoin] = txb.splitCoins(txb.gas, [amountInMist]);
+
+  txb.moveCall({
+    target: "0x2::transfer::public_transfer",
+    typeArguments: ["0x2::sui::SUI"],
+    arguments: [depositCoin, txb.pure(address, "address")],
+  });
+
+  //txb.transferObjects([depositCoin], address);
+
+  suiClient
+    .signAndExecuteTransactionBlock({
+      signer: keypair,
+      transactionBlock: txb,
+      requestType: "WaitForLocalExecution",
+      options: {
+        showEffects: true,
+      },
+    })
+    .then((res) => {
+      console.log(JSON.stringify(res, null, 2));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+testMoveCall(2);
---
+++ scripts/utils.ts
@@ -0,0 +1,28 @@
+import { fromB64 } from "@mysten/bcs";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export function getExecStuff() {
+  if (!process.env.PK_B64) {
+    throw new Error("env var PK_B64 not configured");
+  }
+
+  const b64PrivateKey = process.env.PK_B64 as string;
+  const keypair = Ed25519Keypair.fromSecretKey(fromB64(b64PrivateKey).slice(1));
+  const address = `${keypair.getPublicKey().toSuiAddress()}`;
+
+  if (!process.env.NETWORK) {
+    throw new Error("env var NETWORK not configured");
+  }
+
+  const suiClient = new SuiClient({
+    url: getFullnodeUrl(
+      process.env.NETWORK as "mainnet" | "testnet" | "devnet" | "localnet",
+    ),
+  });
+
+  return { address, keypair, suiClient };
+}
